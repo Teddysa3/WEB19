2f77650637a9b0c44adfd482932f13317aaa6b83
Teddysa <emilie.lefebvre1999@gmail.com>
2020-09-03 16:28:54 +0200

    Modification du fichier de Emilie

--- emilie.php
+++ emilie.php
@@ -1,4 +1,17 @@
-<?php
-    echo '<h1>Ceci n\' est pas le fichier de Emilie</h1>';
-    echo '<h1>Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</h1>';
-?>
+<!DOCTYPE html>
+<header>
+    <title>Auto-entreprise du soleil</title>
+    <link rel="stylesheet" href="style.css">
+
+</header>
+<html>
+<body>
+
+<div style="color:orange;">
+    <h1>Profitez, c'est les vacances !</h1>
+    <p>L'auto-entreprise du soleil s'occupe de tout pendant vos vacances, malgré la solitude de l'employée, ne craignez rien pour la sécurité de vos sites, et détendez-vous pendant vos rares semaines de vacances (peut-être les dernieres...)</p>
+    <h2>VIENDEZ TOUS</h2>
+    <h3>(C'est pas cher en plus)</h3>
+</div>
+</body>
+</html>
